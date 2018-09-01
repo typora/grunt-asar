3d447c9b550266402f30da3a964bba480757b2fa
AbnerLee <ilchenearly@gmail.com>
2018-09-01 23:38:43 +0800

    Update asar.js

--- tasks/asar.js
+++ tasks/asar.js
@@ -1,3 +1,4 @@
+@@ -1,54 +0,0 @@
 /*
  * grunt-asar
  * https://github.com/bwin/grunt-asar
@@ -11,45 +12,16 @@
 var path = require('path');
 var fs = require('fs');
 
+var asar = require('asar');
 var Filesystem = require('asar/lib/filesystem');
 var disk = require('asar/lib/disk');
+var mkdirp = require('mkdirp');
 
 var glob = require('glob');
 
-function generateAsarArchiveFromFiles(basepath, filenames, destFile, cb) {
-  var filesystem = new Filesystem(basepath);
-  var files = [];
-  var i, file, stat, filename;
-
-  for(i in filenames) {
-    filename = filenames[i];
-    if('object' === typeof filename) {
-      filename = filename.src[0];
-    }
-    file = path.join(process.cwd(), filename);
-    stat = fs.lstatSync(file);
-    if(stat.isDirectory()) {
-      filesystem.insertDirectory(file);
-    }
-    else if(stat.isSymbolicLink()) {
-      filesystem.insertLink(file, stat);
-    }
-    else {
-      filesystem.insertFile(file, stat);
-      files.push(file);
-    }
-  }
-
-  disk.writeFilesystem(destFile, filesystem, files, function() {
-    cb(null);
-  });
-}
-
 function generateAsarArchive(srcPath, destFile, cb) {
-  glob(''+srcPath+'/**/*', {}, function(err, entries) {
-    if(err) { return cb(err); }
-    generateAsarArchiveFromFiles(srcPath, entries, destFile, cb);
-  });
+  console.log("from " + srcPath + " to " + destFile);
+  asar.createPackage(srcPath, destFile, cb);
 }
 
 module.exports = function(grunt) {
@@ -62,6 +34,7 @@ module.exports = function(grunt) {
     var target = this.target;
     var done = this.async();
 
+
     var filesLeft = this.files.length;
     var eachDone = function(f) {
       grunt.log.writeln('File ' + f.dest + ' created from ' + f.src[0]);
@@ -70,23 +43,13 @@ module.exports = function(grunt) {
       }
     };
 
-    if(this.data.expand) {
-      var dest = this.data.dest;
+    this.files.forEach(function(f) {
+      var dest = path.join(process.cwd(), f.dest);
+      
+      // to create the dir if necessary we're creating an empty file.
       grunt.file.write(dest, '');
-      generateAsarArchiveFromFiles(this.data.cwd, this.files, this.data.dest, function() {
-        grunt.log.writeln('File ' + dest + ' created.');
-        done();
-      });
-    }
-    else {
-      this.files.forEach(function(f) {
-        var dest = path.join(process.cwd(), f.dest);
-        
-        // to create the dir if necessary we're creating an empty file.
-        grunt.file.write(dest, '');
-        
-        generateAsarArchive(f.src[0], dest, function(){eachDone(f);});
-      });
-    }
+      
+      generateAsarArchive(f.src[0], dest, function(){eachDone(f);});
+    });
   });
 };
